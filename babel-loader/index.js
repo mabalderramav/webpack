b80f41bf6f4f8a972f3e57a2deea3da65f6bf3a0
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2019-04-28 11:47:21 -0400

    Support to EcmaScriptr

---
+++ babel-loader/index.js
@@ -0,0 +1,5 @@
+import './estilos.css';
+import {firstMessage} from './message.js';
+
+document.write(firstMessage);
+console.log('hola mundo! desde un webpack.config');
---
+++ babel-loader/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            // Here are the loader.
+            {
+                // test: que tiempo de archivo va a recibir,
+                // use: que loader se va a encargar del archivo,
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
+                },
+            },
+            {
+               // test: que tiempo de archivo va a recibir,
+               // use: que loader se va a encargar del archivo,
+               test: /\.css$/,
+               use: ExtractTextPlugin.extract({
+               // ['style-loader','css-loader']
+               // fallback: 'style-loader',
+               use: 'css-loader'
+               }),
+            }
+        ]
+    },
+    plugins: [
+        // Here are the loader.
+        new ExtractTextPlugin('css/[name].css')
+    ]
+}
