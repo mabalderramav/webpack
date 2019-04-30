e976759e144c93403ad1d2a20acc0a6420909cd8
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2019-04-29 20:54:52 -0400

    added support to fonts in webpack

---
+++ url-loader-fonts/src/js/index.js
@@ -0,0 +1,13 @@
+import '../css/estilos.css';
+import {firstMessage, delayedMessage} from './message.js';
+import platziImg from  '../images/platzi.png';
+
+document.write(firstMessage);
+delayedMessage();
+
+const img = document.createElement('img');
+img.setAttribute('src',platziImg);
+img.setAttribute('width',50);
+img.setAttribute('height',50);
+document.body.append(img);
+console.log('hola mundo! desde un webpack.config');
