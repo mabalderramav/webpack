51b0ce94051219722e9b1c5aa5dd4d3ce1d477e2
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2019-04-29 21:29:52 -0400

    added support to videos in webpack

---
+++ url-loader-video/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/js/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: './dist/'
+    },
+    module: {
+        rules: [
+            // Here are the loader.
+            {
+                // test: que tiempo de archivo va a recibir,
+                // use: que loader se va a encargar del archivo,
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [["@babel/preset-env", { "targets": { "node": "current" } }]]
+                    }
+                },
+            },
+            {
+                test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/i,
+                use:  {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 100000,
+                    }
+                }
+            },
+            {
+                test: /\.(mp4|webm)$/i,
+                use:  {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 100000,
+                        name: 'videos/[name].[hash].[ext]'
+                    }
+                }
+            },
+            {
+               // test: que tiempo de archivo va a recibir,
+               // use: que loader se va a encargar del archivo,
+               test: /\.css$/,
+               use: ExtractTextPlugin.extract({
+               // ['style-loader','css-loader']
+               // fallback: 'style-loader',
+               use: 'css-loader'
+               }),
+            }
+        ]
+    },
+    plugins: [
+        // Here are the loader.
+        new ExtractTextPlugin('css/[name].css')
+    ]
+}
